50fcf23e0cf02b78739935e659642ae9f79ed0e8
Narisa Kovitaya <bay@baymac.local>
2019-05-16 23:18:22 +0700

    add location, getter, setter to Animal

--- src/main/java/io/muzoo/ooc/ecosystems/Animal.java
+++ src/main/java/io/muzoo/ooc/ecosystems/Animal.java
@@ -1,4 +1,26 @@
 package io.muzoo.ooc.ecosystems;
 
 abstract public class Animal {
+
+    // Individual characteristics (instance fields).
+
+    // The animal's location;
+    private Location location;
+
+    /**
+     * Get the animal's location.
+     * @return The animal's location.
+     */
+    protected Location getLocation() {
+        return location;
+    }
+
+    /**
+     * Set the animal's location.
+     *
+     * @param location The animal's location.
+     */
+    protected void setLocation(Location location){
+        this.location = location;
+    }
 }
