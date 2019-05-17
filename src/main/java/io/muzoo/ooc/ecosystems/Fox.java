47d7b8928ac1debd6d373bf5d218f860b19e6aeb
Narisa Kovitaya <bay@baymac.local>
2019-05-17 10:25:18 +0700

    create Predator abstract class and its constructor

--- src/main/java/io/muzoo/ooc/ecosystems/Fox.java
+++ src/main/java/io/muzoo/ooc/ecosystems/Fox.java
@@ -11,7 +11,7 @@ import java.util.Random;
  * @author David J. Barnes and Michael Kolling
  * @version 2002.10.28
  */
-public class Fox extends Animal{
+public class Fox extends Animal {
     // Characteristics shared by all foxes (static fields).
 
     // The age at which a fox can start to breed.
@@ -29,7 +29,7 @@ public class Fox extends Animal{
     private static final Random rand = new Random();
 
     // Array of the fox's prey.
-    private static final Class[] PREYS = {Rabbit.class};
+    protected static final Class[] PREYS = {Rabbit.class};
 
     // Individual characteristics (instance fields).
 
---
+++ src/main/java/io/muzoo/ooc/ecosystems/Predator.java
@@ -0,0 +1,42 @@
+package io.muzoo.ooc.ecosystems;
+
+abstract public class Predator extends Animal{
+
+    // Individual characteristics (instance fields).
+
+    // The predator's food level, which is increased by eating
+    private int foodLevel;
+
+
+    /**
+     * Create a new animal. An animal may be created with age
+     * zero (a new born) or with a random age.
+     *
+     * @param location  The location of the animal.
+     * @param randomAge If true, the fox will have random age and hunger level.
+     */
+    public Predator(Location location, boolean randomAge, int initialFoodValue) {
+        super(location, randomAge);
+        if (randomAge) {
+            foodLevel = getRand().nextInt(initialFoodValue);
+        } else {
+            foodLevel = initialFoodValue;
+        }
+    }
+
+    protected void incrementHunger() {
+        foodLevel--;
+        if (foodLevel <= 0) {
+            setDead();
+        }
+    }
+
+    protected int getFoodLevel(){
+        return foodLevel;
+    }
+
+    protected void setFoodLevel(int foodLevel) {
+        this.foodLevel = foodLevel;
+    }
+
+}
