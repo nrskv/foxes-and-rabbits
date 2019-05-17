eb1362eab65f5136e5e2550ba242b6cce536d518
Narisa Kovitaya <bay@baymac.local>
2019-05-17 14:08:32 +0700

    create ActorFactory

---
+++ src/main/java/io/muzoo/ooc/ecosystems/ActorFactory.java
@@ -0,0 +1,27 @@
+package io.muzoo.ooc.ecosystems;
+
+public class ActorFactory {
+
+    // The probability that a fox will be created in any given grid position.
+    private static final double FOX_CREATION_PROBABILITY = 0.03;
+    // The probability that a rabbit will be created in any given grid position.
+    private static final double RABBIT_CREATION_PROBABILITY = 0.09;
+    // The probability that a tiger will be created in any given grid position.
+    private static final double TIGER_CREATION_PROBABILITY = 0.01;
+    // The probability that a hunter will be created in any given grid position.
+    private static final double HUNTER_CREATION_PROBABILITY = 0.005;
+
+    public Actor spawnActor(double creationProbability, Location location) {
+        if (creationProbability <= HUNTER_CREATION_PROBABILITY) {
+            return new Hunter(location);
+        } else if (creationProbability <= TIGER_CREATION_PROBABILITY) {
+            return new Tiger(location, true);
+        } else if (creationProbability <= FOX_CREATION_PROBABILITY) {
+            return new Fox(location, true);
+        } else if (creationProbability <= RABBIT_CREATION_PROBABILITY) {
+            return new Rabbit(location, true);
+        }
+        return null;
+    }
+
+}
